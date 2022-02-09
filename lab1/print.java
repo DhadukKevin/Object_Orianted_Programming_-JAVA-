39694fa76b286d3d23db8923a5f466c69a05c7fc
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2022-02-09 20:49:17 +0530

    Add files via upload

---
+++ lab1/print.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class print{
+    public static void main(String[] args)
+    {
+        System.out.print("Input:");
+    Scanner sc=new Scanner(System.in);
+
+    String x=sc.next();
+    System.out.print("Output:");
+    System.out.println(x);
+    }
+}
