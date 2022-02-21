f5385f549b7df564fdfe8bf51e650d7b3ea68fb2
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2022-02-21 23:10:38 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,247 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                int length=s1.length();
+                
+                if(length==1)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"              "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"              "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"              "+"00"+x);
+                    }
+                }
+                if(length==2)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"             "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"             "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"             "+"00"+x);
+                    }
+                }
+                else if(length==3)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"            "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"            "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"            "+"00"+x);
+                    }
+                }
+                else if(length==4)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"           "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"           "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"           "+"00"+x);
+                    }
+                }
+                else if(length==5)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"          "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"          "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"          "+"00"+x);
+                    }
+                }
+                else if(length==6)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"         "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"         "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"         "+"00"+x);
+                    }
+                }
+                else if(length==7)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"        "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"        "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"        "+"00"+x);
+                    }
+                }
+                else if(length==8)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"       "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"       "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"       "+"00"+x);
+                    }
+                }
+                else if(length==9)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"      "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"      "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"      "+"00"+x);
+                    }
+                }
+                else if(length==10)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"     "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"     "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"     "+"00"+x);
+                    }
+                }
+                else if(length==11)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"    "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"    "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"    "+"00"+x);
+                    }
+                }
+                else if(length==12)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"   "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"   "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"   "+"00"+x);
+                    }
+                }
+                else if(length==13)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+"  "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+"  "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+"  "+"00"+x);
+                    }
+                }
+                else if(length==14)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+" "+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+" "+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+" "+"00"+x);
+                    }
+                }
+                else if(length==14)
+                {
+                    if(x>99)
+                    {
+                        System.out.println(s1+""+x);
+                    }
+                    else if(x<100 && x>=10)
+                    {
+                        System.out.println(s1+""+"0"+x);
+                    }
+                    else if(x>=0 && x<10)
+                    {
+                        System.out.println(s1+""+"00"+x);
+                    }
+                }
+                
+                
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
