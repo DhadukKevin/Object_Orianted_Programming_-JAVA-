104a833b8defaf5548ab2a38c1fbc5d1ec755897
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2022-04-30 12:47:36 +0530

    Add files via upload

---
+++ lab8/XYException.java
@@ -0,0 +1,12 @@
+class XYExcepation{
+	public static void main(String[] args){
+		try{
+			int x=Integer.parseInt(args[0]);
+			int y=Integer.parseInt(args[1]);
+			int mul=x*y;
+			System.out.print(mul);
+		}catch(Exception e){
+			System.out.println("Invalid value of x or y");
+		}
+	}
+}
